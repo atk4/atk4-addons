f1c837dc814906b2a0925d51830eddc344c888de
Romans Malinovskis <r@agiletech.ie>
2011-02-03 17:54:25 +0000

    Started implementation of Image model

---
+++ ATK4Addons.php
@@ -0,0 +1,7 @@
+<?
+class addon_atk4addons extends AbstractAddon {
+	// add this into your application to activate addons
+	function init(){
+		parent::init();
+	}
+}
---
+++ misc/lib/View/Filestore/ImageList.php
@@ -0,0 +1,6 @@
+<?php
+class View_Filestore_ImageList extends MVCLister {
+	function defaultTemplate(){
+		return array('view/filestore/imagelist');
+	}
+}
