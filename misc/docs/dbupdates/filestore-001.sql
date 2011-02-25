7ad255972b08701c0d817bd0fcb79e61b72cbe73
Romans Malinovskis <r@agiletech.ie>
2011-02-25 14:48:06 +0000

    Symlink files form here into your local dbupdates

---
+++ misc/docs/dbupdates/filestore-001.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `filestore_image` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255),
+  `original_file_id` int(11) NOT NULL DEFAULT '0',
+  `thumb_file_id` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
