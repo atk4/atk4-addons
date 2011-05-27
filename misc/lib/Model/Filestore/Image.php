2d622bb8f3095d12e371fe3ead5fd38dbc05f9ca
Romans Malinovskis <r@agiletech.ie>
2011-05-27 16:51:00 +0100

    Finish Image coding

--- misc/lib/Model/Filestore/Image.php
+++ misc/lib/Model/Filestore/Image.php
@@ -1,6 +1,8 @@
 <?php
 class Model_Filestore_Image extends Model_Filestore_File {
 	//protected $entity_code='filestore_image';
+	public $default_thumb_height=140;
+	public $default_thumb_width=140;
 
 	// Temporarily, to be replaced in 4.1 to use Model_File
 	public $entity_file='Filestore_File';
@@ -35,21 +37,7 @@ class Model_Filestore_Image extends Model_Filestore_File {
 
         $original=$this->getPath();
 
-        // Create entry for thumbnail.
-        $thumb=$this->getRef('thumb_file_id');
-        if(!$thumb->isInstanceLoaded()){
-            $thumb->set('filestore_volume_id',$this->get('filestore_volume_id'));
-            $thumb->set('original_filename','thumb_'.$this->get('original_filename'));
-            $thumb->set('filestore_type_id',$this->get('filestore_type_id'));
-            $thumb->update();
-            $this->set('thumb_file_id',$thumb->get('id'));
-        }
-
-        $image=new Imagick($original->getPath());
-        $image->resizeImage(100,100,Imagick::FILTER_LANCZOS,1,true);
-        $image->writeImage($thumb->getPath());
-
-        $thumb->import(null,'none')->update();  // update size and chmod
+		$this->createThumbnail('thumb_file_id',$this->default_thumb_height,$this->default_thumb_width);
 
 		// Now that the origninal is imported, lets generate thumbnails
         /*
@@ -58,6 +46,24 @@ class Model_Filestore_Image extends Model_Filestore_File {
 		$this->afterImport();
         */
         return $this;
+	}
+	function createThumbnail($field,$x,$y){
+        // Create entry for thumbnail.
+        $thumb=$this->getRef($field);
+        if(!$thumb->isInstanceLoaded()){
+            $thumb->set('filestore_volume_id',$this->get('filestore_volume_id'));
+            $thumb->set('original_filename','thumb_'.$this->get('original_filename'));
+            $thumb->set('filestore_type_id',$this->get('filestore_type_id'));
+            $thumb->update();
+            $this->set($field,$thumb->get('id'));
+        }
+
+        $image=new Imagick($this->getPath());
+        $image->resizeImage($x,$y,Imagick::FILTER_LANCZOS,1,true);
+        $image->writeImage($thumb->getPath());
+
+        $thumb->import(null,'none');
+		$thumb->update();  // update size and chmod
 	}
 	function afterImport(){
 		// Called after original is imported. You can do your resizes here
