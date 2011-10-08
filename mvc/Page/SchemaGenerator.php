e2805dd91cebd0245e421a93da4c2de24b427017
Romans Malinovskis <r@agiletech.ie>
2011-10-08 23:24:34 +0100

    fix tab

--- mvc/Page/SchemaGenerator.php
+++ mvc/Page/SchemaGenerator.php
@@ -24,7 +24,7 @@ class Page_SchemaGenerator extends Page {
         }
     }
     function init(){
-		parent::init();
+        parent::init();
         /* dirty. will clean up later, but working well */
         $c=$this->add('Columns');
         $f=$c->addColumn('50%')->add('Form');
