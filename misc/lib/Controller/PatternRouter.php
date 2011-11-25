3e4d790e3836cc613ffb8ab90edb9e23f4eab23c
Janis <j@agiletech.ie>
2011-11-25 03:19:19 +0200

    PatterRouter updates

--- misc/lib/Controller/PatternRouter.php
+++ misc/lib/Controller/PatternRouter.php
@@ -6,22 +6,30 @@
  *
  * in Frontend:
  *
- * $r = $this->add("Router")->addRule("(news\/.*)", "news_item", array("u" => 
- * 1))->route();
+ * $r = $this->add("Controller_PatternRouter")->addRule("(news\/.*)", "news_item", array("u"))->route();
  *
  * if REQUEST_URI is "/news/some-name-of-your-news/", then router would:
  * 1) set $this->api->page to "news_item"
  * 2) set $_GET["u"] to "news/some-name-of-your-news/"
- * 3) set $_GET["rp"] to array containing all matched values in the request 
  * uri.
  *
  * enjoy. j. 2011
  *
  * */
-class Router extends AbstractController {
+class Controller_PatternRouter extends AbstractController {
     private $rules;
     function addRule($regex, $target=null, $params=null){
         $this->rules[] = array($regex, $target, $params);
+        return $this;
+    }
+    function setModel($model){
+        if (!is_object($model)){
+            $model = $this->add("Model_" . $model);
+        }
+        $rules = $model->getRows();
+        foreach ($rules as $rule){
+            $this->addRule($rule["rule"], $rule["target"], explode(",", $rule["params"]));  
+        }
         return $this;
     }
     function route(){
@@ -29,10 +37,9 @@ class Router extends AbstractController {
         foreach ($this->rules as $rule){
             if (preg_match("/" . $rule[0] . "/", $r, $t)){
                 $this->api->page = $rule[1];
-                $_GET["rp"] = $t;
                 if ($rule[2]){
                     foreach ($rule[2] as $k => $v){
-                        $_GET[$k] = $t[$v];
+                        $_GET[$v] = $t[$k+1];
                     }
                 }
             }
