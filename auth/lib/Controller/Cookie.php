d34737b58a919680a3e5804321a4eb3739532498
Romans Malinovskis <r@agiletech.ie>
2012-03-29 16:10:31 +0100

    fixes

---
+++ auth/lib/Controller/Cookie.php
@@ -0,0 +1,40 @@
+<?php
+namespace auth;
+/** Enhances authentication procedure by adding ability to set cookie on login. Manually logging out will clear cookie. */
+class Controller_Cookie extends \AbstractController{
+    function init(){
+        parent::init();
+        //var_dump($_COOKIE);
+        $this->api->requires('atk','4.2');
+
+        if(!$this->owner instanceof \BasicAuth){
+            throw $this->exception('Must be added into $api->auth');
+        }
+
+        $this->owner->addHook(array('check','loggedIn','logout','updateForm'),$this);
+    }
+    function check($auth){
+        if(isset($_COOKIE[$auth->name."_username"]) && isset($_COOKIE[$auth->name."_password"])){
+
+            $id=$auth->verifyCredintials( $_COOKIE[$auth->name."_username"], $_COOKIE[$auth->name."_password"]);
+            if($id){
+                // Successfully validated user
+                $this->breakHook($id);
+            }
+        }
+    }
+    function loggedIn($auth,$user=null,$pass=null){
+        if(!$pass)return;
+        if(!$auth->form->get('memorize'))return;
+
+        setcookie($auth->name."_username",$user,time()+60*60*24*30*6);
+        setcookie($auth->name."_password",$pass,time()+60*60*24*30*6);
+    }
+    function logout($auth){
+		setcookie($auth->name."_username",null);
+		setcookie($auth->name."_password",null);
+    }
+    function updateForm($auth){
+		$auth->form->addField('Checkbox','memorize','Remember me');
+    }
+}
---
+++ auth/lib/Controller/DummyPopup.php
@@ -0,0 +1,41 @@
+<?php
+namespace auth;
+/** Dummy sample addon which shows you popup with list of all users. Clicking on a user will
+ * resume login process */
+class Controller_DummyPopup extends \AbstractController{
+    function init(){
+        parent::init();
+        $this->api->requires('atk','4.2');
+
+        if(!$this->owner instanceof\ BasicAuth){
+            var_dump(get_class($this->owner));
+            throw $this->exception('Must be added into $api->auth');
+        }
+
+        $this->owner->addHook(array('updateForm'),$this);
+
+        if($_GET[$this->name]=='popup'){
+
+            if($_GET['user']){
+                $this->owner->loginByID($_GET['user']);
+                echo "<script>window.opener.document.location=window.opener.document.location;window.close()</script>";
+            }
+
+            $this->api->stickyGET($this->name);
+            $l=$this->api->add('auth/UserLister');
+            $l->setModel($this->owner->model);
+            echo $l->getHTML();exit;
+        }
+
+    }
+    function updateForm($auth){
+        $b=$auth->form->addButton('Pick a User');
+        $b->js('click')->univ()->newWindow($this->api->url(null,array($this->name=>'popup')),'auth','height=500,width=500');
+    }
+}
+
+class UserLister extends \Lister {
+    function formatRow(){
+        $this->current_row_html['name']='<a href="'.$this->api->url(null,array('user'=>$this->current_id)).'">'.$this->current_row['name'].'</a>';
+    }
+}
