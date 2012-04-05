51d3390c8d0e6edb4ef04d8ad317e412a8a21863
Romans Malinovskis <r@agiletech.ie>
2012-04-05 18:22:05 +0100

    improve profiler's output and accuracy
    - will show when it's started and when finished
    - put add() of profiler when you need to start profiling. destroy profiler when you need to stop
    - profiler will now track time how much it adds in overheads
    - re-enabled checking of matching start/stop by default

--- performance/lib/Controller/Profiler.php
+++ performance/lib/Controller/Profiler.php
@@ -6,10 +6,15 @@ class Controller_Profiler extends \AbstractController {
     public $timetable=array();
     public $curfunc=array();
     public $offset=0;
+    public $profstart=null;
+    public $calls=0;
 
     function init(){
         parent::init();
         $this->api->pr=$this;
+
+        $this->profstart=time()+microtime();
+        $this->start('everything else');
     }
 
     /** Returns microseconds since last call */
@@ -29,7 +34,6 @@ class Controller_Profiler extends \AbstractController {
         $this->start($location);
     }
     function getFlatStack(){
-        return '';
         $bt=debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS);
         $fx=array();
         foreach($bt as $row){
@@ -40,27 +44,40 @@ class Controller_Profiler extends \AbstractController {
     }
     function start($location=null){
         if($this->curfunc)$this->logTime($this->curfunc[0][0]);
+        else $this->getOffset();
         array_unshift($this->curfunc,array($location,$this->getFlatStack()));
         $this->timetable[$location]['calls']++;
+        $this->logTime('Profiling');
     }
     function stop($limit=null,$ignore_fx=false){
+        $this->calls++;
         if(!$this->curfunc){
             throw $this->exception('Stopping but wasn\'t started');
         }
         list($location,$stack)=array_shift($this->curfunc);
+        $this->logTime($location,$limit);
+
         $newstack=$this->getFlatStack();
         if($stack!=$newstack && !$ignore_fx)throw $this->exception('Trace start and stop are in different functions. use stop(null,true); if that is desired')
             ->addMoreInfo('started_at',$stack)
             ->addMoreInfo('stopped_at',$newstack)
             ->addMoreInfo('location',$location)
             ;
-        $this->logTime($location,$limit);
+        $this->logTime('Profiling');
+    }
+    function destroy(){
+        $this->api->pr=new \Dummy();
+        return parent::destroy();
     }
     function dump(){
         if($this->curfunc)var_Dump($this->curfunc);
 
         
-        echo "Total: ".(time()+microtime()-$this->api->start_time).'<br/>';
+        echo "Profiling period: ".($this->profstart-$this->api->start_time).' .. '.
+            (time()+microtime()-$this->api->start_time).'<br/>';
+        echo "Profiled (ms): <b>".round(1000*(time()+microtime()-$this->profstart
+            -$this->timetable['Profiling']['total']
+        ),2).'</b><br/>';
 
         uasort($this->timetable,function($l,$r){
             $l=$l['total'];///$l['calls'];
@@ -69,11 +86,18 @@ class Controller_Profiler extends \AbstractController {
             if($l<$r)return 1;
         });
 
+        $x=0;
         foreach($this->timetable as $activity=>$detail){
+            if($activity=='Profiling'){
+                $activity='<font color="gray">'.$activity.'</font>';
+                $detail['calls']=$this->calls*2;
+            }
+            $x+=$detail['total'];
             echo '+'.number_format($detail['total']/*/$detail['calls']*/*1000,2).' '.$activity.' x '.$detail['calls'].'<br/>';
         }
     }
     function __destruct(){
+        $this->stop(null,true);
         $this->dump();
     }
 }
