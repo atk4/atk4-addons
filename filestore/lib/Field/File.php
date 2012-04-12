05ba8a2c790e43b6cb70d8b17ad5952b6b69448c
Romans Malinovskis <r@agiletech.ie>
2012-04-12 09:07:53 +0100

    fix the fix

--- filestore/lib/Field/File.php
+++ filestore/lib/Field/File.php
@@ -8,13 +8,11 @@ class Field_File extends \Field {
         $this->display(array('form'=>'upload'));
     }
     function displaytype($x){return $this;}
-    function ref($load=null){
+    function ref(){
         if(!$this->model){
-            $this->model=preg_replace('|^(.*/)?(.*)$|','\1Model_\2',$this->getModel());
+            $this->model=preg_replace('|^(.*/)?(.*)$|','\1Model_\2',$this->use_model);
             $this->model=$this->add($this->model);
         }
-        if($load === false)return $this->model;
-        if($load === null)return $this->model->tryLoad($this->get());
         if(!$this->get())throw $this->exception('Reference field has no value')
             ->addMoreInfo('model',$this->owner)
             ->addMoreInfo('field',$this->short_name)
