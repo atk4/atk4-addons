994ac453faece4453ecca915360c6a3ff2c0a3a3
Romans Malinovskis <r@agiletech.ie>
2012-04-17 09:36:27 +0100

    add PHP callback functionality

---
+++ misc/lib/Field/Callback.php
@@ -0,0 +1,28 @@
+<?php
+namespace misc;
+class Field_Callback extends \Field {
+    public $callback=null;
+    public $initialized=false;
+    function init(){
+        parent::init();
+        $this->editable(false);
+    }
+    function set($callback){
+        $this->callback=$callback;
+    }
+    function updateSelectQuery($select){
+        $this->initialized=true;
+        $this->owner->addHook('afterLoad',$this);
+    }
+    function afterLoad($m){
+        $result=call_user_func($this->callback,$this->owner,$this);
+        $this->owner->set($this->short_name,$result);
+        return $this;
+    }
+    function updateInsertQuery($insert){
+        return $this;
+    }
+    function updateModifyQuery($insert){
+        return $this;
+    }
+}
