8223da67188ca8153994153b8776566812b2bd4c
Romans Malinovskis <r@agiletech.ie>
2012-08-28 01:16:40 +0100

    Compatibility with 4.2.1

--- misc/lib/InfiniteAddForm.php
+++ misc/lib/InfiniteAddForm.php
@@ -1,14 +1,28 @@
 <?php
-class InfiniteAddForm extends View {
+/* 
+Copyright (C) <2012+> Romans Malinovskis <romans@agiletoolkit.org>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy,
+modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software
+is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+namespace misc;
+class InfiniteAddForm extends \View {
     /*
        Infinite add form for specified model
        */
-    public $c;
     public $form;
-    function setController($c){
-
-        $this->c=$c;
-        
+    function setModel($m){
+        parent::setModel($m);
         $this->addForm($_GET[$this->name]?$_GET[$this->name]:1);
     }
     function addForm($u){
@@ -18,16 +32,13 @@ class InfiniteAddForm extends View {
         $this->api->stickyGET($this->name);
 
         // Horizontal form is good for 4 fields. Most of the behaviour can be changed through Controller
-        $this->form=$f=$this->add('MVCForm',$u,null,array('form_horizontal'));
-        $f->setController($this->c);
-
-        // No buttons
-        if($f->hasElement('Save'))$f->elements['Save']->destroy();
+        $this->form=$f=$this->add('Form',$u,null,array('form_horizontal'));
+        $f->setModel($this->model);
 
         // Determine first and last field in form
         $first_field=null;
         foreach($f->elements as $element){
-            if(!($element instanceof Form_Field))continue;
+            if(!($element instanceof \Form_Field))continue;
             $element->js(true)->univ()->disableEnter();
             if(!$first_field)$first_field=$element;
             $last_field=$element;
@@ -41,7 +52,7 @@ class InfiniteAddForm extends View {
         // by the time this form is filled out. Also drop binding to avoid double-loading
         $first_field->js('focus',array(
                 $this->js()->append('<div id="'.$un.'"/>'),
-                $this->js()->_selector('#'.$un)->atk4_load($this->api->getDestinationURL(null,
+                $f->js()->_selector('#'.$un)->atk4_load($this->api->getDestinationURL(null,
                         array($this->name=>$u+1,'cut_object'=>$un))),
                 $first_field->js()->unbind('focus'),
              ));
