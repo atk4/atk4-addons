1c7bbae1c840a27a3f6e10db3d4c01b30ee1ed94
Janis Volbergs <j@agiletech.ie>
2012-11-12 19:23:14 +0200

    Better

--- misc/lib/Controller/AutoPaginator.php
+++ misc/lib/Controller/AutoPaginator.php
@@ -9,17 +9,16 @@ class Controller_AutoPaginator extends \AbstractController {
         $this->limit = $limit;
         $this->owner->dq->limit($limit);
         $g = $this->owner;
-        if (isset($_GET[$g->name . "_more"])){
-            $offset = $_REQUEST["offset"];
-            $g->dq->limit($limit, $offset);
+        if (isset($_REQUEST[$g->name . "_offset"])){
+            $offset = $_REQUEST[$g->name . "_offset"];
+            $g->dq->limit($limit + $offset);
             $g->show_header = false;
-            $g->addHook("output", array($this, "trap"));
-            $g->recursiveRender();
-            $g->js(null, $g->js()->attr("offset", $limit)->append($this->out))->execute();
+            $g->js(true)->attr("offset", $limit + $offset);
         } else {
             $g->js(true)->attr("offset", 0);
         }
-        $g->js(true, "$(window).scroll(function(){if ($(window).scrollTop() + $(window).height() == $(document).height()){" . $g->js()->univ()->ajaxec($this->api->url(null, array($g->name . "_more" => true)), array("offset" => $g->js()->attr("offset"))) . "}});");
+        //$g->js(true, "$(window).scroll(function(){if ($(window).scrollTop() + $(window).height() == $(document).height()){" . $g->js()->univ()->ajaxec($this->api->url(null, array($g->name . "_more" => true)), array("offset" => $g->js()->attr("offset"))) . "}});");
+        $g->js(true, "$(window).scroll(function(){if ($(window).scrollTop() + $(window).height() == $(document).height()){" . $g->js()->reload(array($g->name . "_offset" => $g->js()->attr("offset"))) . "}});");
     }
     function trap($o, $what){
         $this->out .= $what;
