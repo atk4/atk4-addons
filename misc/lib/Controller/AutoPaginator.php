0738b9775829c1f27c93b71ffa89e9e3398e329c
Janis Volbergs <j@agiletech.ie>
2012-11-14 16:13:57 +0200

    Fix

--- misc/lib/Controller/AutoPaginator.php
+++ misc/lib/Controller/AutoPaginator.php
@@ -12,15 +12,10 @@ class Controller_AutoPaginator extends \AbstractController {
         if (isset($_REQUEST[$g->name . "_offset"])){
             $offset = $_REQUEST[$g->name . "_offset"];
             $g->dq->limit($limit + $offset);
-            $g->show_header = false;
             $g->js(true)->attr("offset", $limit + $offset);
         } else {
             $g->js(true)->attr("offset", 0);
             $g->js(true, "$(window).scroll(function(){if ($(window).scrollTop() + $(window).height() == $(document).height()){" . $g->js()->reload(array($g->name . "_offset" => $g->js()->attr("offset"))) . "}});");
         }
-        //$g->js(true, "$(window).scroll(function(){if ($(window).scrollTop() + $(window).height() == $(document).height()){" . $g->js()->univ()->ajaxec($this->api->url(null, array($g->name . "_more" => true)), array("offset" => $g->js()->attr("offset"))) . "}});");
-    }
-    function trap($o, $what){
-        $this->out .= $what;
     }
 }
