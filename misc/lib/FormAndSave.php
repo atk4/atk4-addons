0ac37d5e24b25961886e94ca887d4e80bd8d0a9b
Imants Horsts <imants.horsts@inbox.lv>
2013-01-19 12:56:41 +0200

    FormAndSave: model update should happen later
    Model update should happen not in initialization moment, but later.
    Otherwise we can get situation, that model is not yet initialized when f->update() executes.
    
    For example,
    $model = $page->add('Model_Foo');
    $form = $page->add('FormAndSave');
    $form->setModel($model);
    submitting will not work because model is associated to form after form initialization.
    This happens at least in situation when we use Model with Session or Array controller.

--- misc/lib/FormAndSave.php
+++ misc/lib/FormAndSave.php
@@ -3,10 +3,11 @@ class FormAndSave extends MVCForm {
     function init(){
         parent::init();
         $this->addSubmit('Save');
-
+        $this->api->addHook("pre-render", array($this, "preRender"));
+    }
+    function preRender(){
         $this->onSubmit(function($f){
             $f->update();
-
             $f->js()->univ()->successMessage('Saved')->execute();
         });
     }
