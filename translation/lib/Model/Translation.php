860ccb7d2acd392bf74d735396ab1d3a54556e15
Romans Malinovskis <r@agiletech.ie>
2013-01-21 09:03:09 +0000

    fix translations

--- translation/lib/Model/Translation.php
+++ translation/lib/Model/Translation.php
@@ -1,14 +1,14 @@
 <?php
 namespace translation;
 class Model_Translation extends \Model_Table {
-	public $table="translation";
-	function init(){
-		parent::init();
-		
-		$this->addField('key')->system(true)->visible(true)->editable(true)->readonly(true);
-		$this->addField('tr_en');
-		$this->addField('tr_de');
-		$this->addField('tr_ru');
-		$this->addField('tr_lv');	// extend and add your language
-	}
+    public $table="translation";
+    function init(){
+        parent::init();
+
+        $this->addField('key')->system(true)->visible(true)->editable(true)->readonly(true)->sortable(true);
+        $this->addField('tr_en')->sortable(true);
+        $this->addField('tr_de')->sortable(true);
+        $this->addField('tr_ru')->sortable(true);
+        $this->addField('tr_lv')->sortable(true);	// extend and add your language
+    }
 }
