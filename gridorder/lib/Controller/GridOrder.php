a97f042dec5809c981ebb304c5e1438553e08f7c
Imants Horsts <imants.horsts@inbox.lv>
2013-02-01 17:42:51 +0200

    Update gridorder/lib/Controller/GridOrder.php

--- gridorder/lib/Controller/GridOrder.php
+++ gridorder/lib/Controller/GridOrder.php
@@ -6,41 +6,46 @@ class Controller_GridOrder extends \AbstractController {
     function init(){
         parent::init();
 
-        $this->api->requires('atk','4.2');
+        // requirements
+        $this->api->requires('atk','4.2.3');
 
-        $l=$this->api->locate('addons','gridorder','location');
-        $this->api->pathfinder->addLocation($this->api->locate('addons','gridorder'),array(
+        // add locations
+        $l=$this->api->locate('addons',__NAMESPACE__,'location');
+        $addon = $this->api->locate('addons',__NAMESPACE__);
+        $this->api->pathfinder->addLocation($addon,array(
             'template'=>'templates'
         ))->setParent($l);
 
+        // add button
         $this->owner->addButton('Re-order records')
             ->js('click')->univ()->frameURL('Re-order records',
                     $this->api->url(null,
                         array($this->name=>'activate')),array('width'=>'500px'));
 
-        $this->owner->dq->order('ord');
-
+        // save model
+        $this->model = $this->owner->model;
+        if(!$this->model->hasElement('ord')){
+            $this->model->addField('ord')->system(true);
+        }
+        $this->model->setOrder('ord');
+        
+        // open dialog on button click
         if($_GET[$this->name]=='activate'){
             $this->api->stickyGET($this->name);
             $this->initFrame();
         }
     }
     function initFrame(){
-        $v=$this->owner->owner->add('View',null,$this->owner->spot);
-        $_GET['cut_object']=$v->name;
-
-        $lister=$v->add('CompleteLister',null,null,array('view/gridorder'));
-        $this->model=$m=$this->owner->getModel();
-
-        if(!$m->hasElement('ord')){
-            $m->addField('ord')->system(true);
-        }
-
-        $lister->setModel($m);
-        $lister->dq->order('ord');
-
+        // add view
+        $v = $this->owner->owner->add('View',null,$this->owner->spot);
+        $this->api->cut($v);
+        
+        // add lister
+        $lister = $v->add('CompleteLister',null,null,array('view/gridorder'));
+        $lister->setModel($this->model);
         $lister->js(true)->sortable();
 
+        // add save button and action
         $v->add('Button')->set('Save')->js('click')->univ()->ajaxec(
                 array($this->api->url(),
                 $this->name.'_order'=>$v->js(null,"\$('#{$lister->name}').children().map(function(){ return $(this).attr('data-id'); }).get().join(',')")
@@ -52,29 +57,32 @@ class Controller_GridOrder extends \AbstractController {
         }
     }
     function processReorder($id_order){
+        
         // add missing "ord" fields
-        $q=$this->model->dsql();
-        $q->set('ord',$q->expr('id'));
-        $q->where($q->expr('ord is null'));
-        $q->do_update();
+        $this->model->dsql()
+            ->set('ord',$this->model->id_field)
+            ->where('ord',null)
+            ->do_update();
 
-        $q=$this->model->dsql()->field($this->model->table.'.id')->field('ord');
-        $seq=$q->do_getAllHash();
+        // get current order sequence
+        $seq = $this->model->dsql()
+            ->field($this->model->getElement($this->model->id_field)->getExpr())
+            ->field($this->model->getElement('ord'))
+            ->get();
 
-        // extract ORDs
-        $ord=array();
+        // extract ORDs and sort them
+        $ord = array();
         foreach($seq as $key=>$val){
-            $ord[]=$val['ord'];
+            $ord[] = $val['ord'];
         }
-
         sort($ord);
         
+        // update database each record one by one
         foreach(explode(',',$id_order) as $id){
-            $q=$this->model->dsql();
-            $q->set('ord',array_shift($ord));
-            $q->where('id',$id);
-            $q->do_update();
+            $this->model->dsql()
+                ->set('ord',array_shift($ord))
+                ->where($this->model->id_field,$id)
+                ->do_update();
         }
     }
 }
-
