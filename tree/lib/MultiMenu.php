8dfff253e45d11e9b9b25afa1e83759bd58d82d6
Janis Volbergs <j@agiletech.ie>
2013-06-10 20:21:02 +0300

    Easier to pass extra params

--- tree/lib/MultiMenu.php
+++ tree/lib/MultiMenu.php
@@ -2,13 +2,13 @@
 namespace tree;
 class MultiMenu extends TreeView {
     public $use_template = "treemenu";
-    function addMenuItem($page, $title, $ref = null){
+    function addMenuItem($page, $title, $ref = null, $aux=array()){
         $current = false;
         $p = (string)$this->api->url($page);
         if ((string)$this->api->url(null) == $p){
             $current = true;
         }
-        return $this->addItem(array("page" => $p, "title" => $title, "current" => $current), $ref);
+        return $this->addItem(array("page" => $p, "title" => $title, "current" => $current)+$aux, $ref);
     }
     function defaultTemplate(){
         return array("multimenu");
