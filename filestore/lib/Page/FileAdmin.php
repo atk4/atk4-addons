b7307068974555d8248e78bea9fb76ce0cb25e95
Imants Horsts <DarkSide666@users.noreply.github.com>
2015-01-28 19:58:35 +0200

    Add "open" button in files grid
    Adds "Open" button in Files CRUD.
    When you click on this button, dialog pops up and it embeds HTML object with your file.
    It works very nice on new browsers and with PDF files. Not sure about other type of files yet.

--- filestore/lib/Page/FileAdmin.php
+++ filestore/lib/Page/FileAdmin.php
@@ -71,10 +71,33 @@ class Page_FileAdmin extends \Page
                     ? array('original_filename', 'filename', 'filesize', 'dirname', 'url', 'filestore_type_id')
                     : array('original_filename', 'filename', 'filesize', 'dirname', 'url', 'filestore_type', 'deleted')
             )->setOrder('id desc');
-        if ($g->grid) {
+
+        if (!$g->isEditing()) {
+            // add paginator
             $g->grid->addPaginator(50);
+            
+            // add open button
+            $self = $this;
+            $g->grid->add('VirtualPage')
+                ->addColumn('open', 'View file', 'Open')
+                ->set(function($page)use($self){
+                    $id = $_GET[$page->short_name.'_id'];
+
+                    // find Filestore file
+                    $m = $page->add($self->file_model_class)->load($id);
+                    
+                    // open as object
+                    $page->add('View')
+                        ->setElement('object')
+                        ->setAttr('type', $m->ref('filestore_type_id')->get('mime_type'))
+                        ->setAttr('data', $m->get('url'))
+                        ->setAttr('width', '100%')
+                        ->setAttr('height', '500px')
+                        ->setHTML('Your browser is to old to open file '.$m->get('url'))
+                        ;
+                });
         }
-        
+
         // Image list (show only if we use Model_Image class as file model)
         $m = $this->add($this->file_model_class);
         if ($m instanceof Model_Image) {
@@ -83,5 +106,5 @@ class Page_FileAdmin extends \Page
             $v->setModel($m)->setOrder('id desc');
             $v->addPaginator(20);
         }
-	}
+    }
 }
